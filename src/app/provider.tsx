12c89c5432fab99d4d530a33876989c29649e077
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-03-22 22:55:58 -0300

    feat: auth context

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,24 +1,45 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/configs/firebaseConfig";
+import { AuthContext } from "./_context/AuthContext";
 
 interface ProviderProps {
   children: React.ReactNode;
 }
 
 const Provider: React.FC<ProviderProps> = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="dark"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </NextThemesProvider>
+      <AuthContext.Provider value={{ user }}>
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </NextThemesProvider>
+      </AuthContext.Provider>
     </div>
   );
+};
+
+export const useAuthContext = () => {
+  const context = React.useContext(AuthContext);
+
+  return context;
 };
 
 export default Provider;
