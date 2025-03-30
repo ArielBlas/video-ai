1e9ba50092a88f6a40b0fa4c5a0733ef0ac3015d
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-03-29 23:52:24 -0300

    feat: topic component

--- src/app/(main)/create-new-video/_components/Topic.tsx
+++ src/app/(main)/create-new-video/_components/Topic.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+"use client";
+import { Input } from "@/components/ui/input";
+import React, { useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+
+const sugesstions = [
+  "Historic Story",
+  "Kids Story",
+  "Movie Stories",
+  "AI Innovations",
+  "Space Mysteries",
+  "Horror Stories",
+  "Mythological Tales",
+  "Tech Breakthroughs",
+  "True Crime Stories",
+  "Fantasy Adventures",
+  "Science Experiments",
+  "Motivational Stories",
+];
 
 const Topic = () => {
-  return <div>Topic</div>;
+  const [selectedTopic, setSelectedTopic] = useState("");
+  return (
+    <div>
+      <h2 className="mb-1">Project Title</h2>
+      <Input placeholder="Enter project title" />
+
+      <div className="mt-5">
+        <h2>Video Topic</h2>
+        <p className="text-sm text-gray-600">Select topic for your video</p>
+
+        <Tabs defaultValue="sugesstion" className="w-full mt-2">
+          <TabsList>
+            <TabsTrigger value="sugesstion">Suggestions</TabsTrigger>
+            <TabsTrigger value="your_topic">Your Topic</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sugesstion">
+            <div className="flex flex-wrap gap-2">
+              {sugesstions.map((suggestion, index) => (
+                <Button
+                  key={index}
+                  variant="outline"
+                  className={`${selectedTopic === suggestion ? "bg-secondary" : ""}`}
+                  onClick={() => setSelectedTopic(suggestion)}
+                >
+                  {suggestion}
+                </Button>
+              ))}
+            </div>
+          </TabsContent>
+          <TabsContent value="your_topic">
+            <div>
+              <h2>Enter your own topic</h2>
+              <Textarea placeholder="Enter your topic" />
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Topic;
--- src/app/(main)/create-new-video/page.tsx
+++ src/app/(main)/create-new-video/page.tsx
@@ -5,14 +5,19 @@ const CreateNewVideo = () => {
   return (
     <div>
       <h2 className="text-3xl">Create New Video</h2>
-      {/* Topic & Script */}
-      <Topic />
+      <div className="grid grid-cols-1 md:grid-cols-3 mt-8">
+        <div className="col-span-2 p-7 border rounded-xl">
+          {/* Topic & Script */}
+          <Topic />
 
-      {/* Video Image Style */}
+          {/* Video Image Style */}
 
-      {/* Voice */}
+          {/* Voice */}
 
-      {/* Captions */}
+          {/* Captions */}
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
