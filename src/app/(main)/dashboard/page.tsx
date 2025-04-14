762eb102f6084caad5056870ee6b1ee5bbc5f41e
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-04-13 23:00:59 -0300

    feat: video list component

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import VideoList from "./_components/VideoList";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h3 className="font-bold text-3xl">My Videos</h3>
+      <VideoList />
+    </div>
+  );
 };
 
 export default Dashboard;
