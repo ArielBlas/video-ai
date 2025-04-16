170bb68e8a76901a99ac040a1a29cd179a3c8af2
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-04-15 23:09:40 -0300

    feat: card video

--- src/app/(main)/dashboard/_components/VideoList.tsx
+++ src/app/(main)/dashboard/_components/VideoList.tsx
@@ -6,6 +6,8 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { api } from "../../../../../convex/_generated/api";
 import { useAuthContext } from "@/app/provider";
+import moment from "moment";
+import { RefreshCcw } from "lucide-react";
 
 const VideoList = () => {
   const [videoList, setVideoList] = useState([]);
@@ -27,7 +29,7 @@ const VideoList = () => {
 
   return (
     <div>
-      {videoList.length > 0 && (
+      {videoList.length == 0 ? (
         <div className="flex flex-col items-center justify-center mt-28 gap-5 p-5 border border-dashed rounded-xl py-16">
           <Image src={"/logo.svg"} alt="logo" width={60} height={60} />
           <h2 className="text-gray-400 text-lg">
@@ -36,6 +38,37 @@ const VideoList = () => {
           <Link href="/create-new-video">
             <Button>+ Create new video</Button>
           </Link>
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-5 mt-10">
+          {videoList.map((video, index) => (
+            <div className="relative" key={index}>
+              {video.status == "completed" ? (
+                <Image
+                  src={video?.images[0]}
+                  alt={video?.title}
+                  width={500}
+                  height={500}
+                  className="w-full object-cover rounded-xl aspect-[2/3]"
+                />
+              ) : (
+                <div
+                  className="aspect-[2/3] p-5 w-full rounded-xl bg-slate-900 
+                  flex items-center justify-center gap-2"
+                >
+                  <RefreshCcw className="animate-spin" />
+                  <h2>Generating...</h2>
+                </div>
+              )}
+
+              <div className="absolute bottom-3 px-5 w-full">
+                <h2>{video?.title}</h2>
+                <h2 className="text-sm">
+                  {moment(video?._creationTime).fromNow()}
+                </h2>
+              </div>
+            </div>
+          ))}
         </div>
       )}
     </div>
